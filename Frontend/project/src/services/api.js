d55c13285570780b5895dcd4c6ef681f881dd436
Zaid khan <itszaidkhan01@gmail.com>
2026-01-03 21:25:20 +0530

    Fix SPA routing for Vercel

--- Frontend/project/src/services/api.js
+++ Frontend/project/src/services/api.js
@@ -1,19 +1,22 @@
 import axios from "axios";
 
+// Replace this with your actual deployed backend URL
+const API_BASE_URL = "https://contact-management-9yc1.onrender.com/api"; 
+
 const API = axios.create({
-  baseURL: "http://localhost:8000/api", 
+  baseURL: API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 // CREATE CONTACT
-export const createContact = (data) => API.post("/contacts/", data);
+export const createContact = (data) => API.post("/contacts", data);
 
 // GET CONTACTS
-export const getContacts = () => API.get("/contacts/");
+export const getContacts = () => API.get("/contacts");
 
 // DELETE CONTACT 
-export const deleteContact = (id) => API.delete(`/contacts/${id}/`);
+export const deleteContact = (id) => API.delete(`/contacts/${id}`);
 
 export default API;
